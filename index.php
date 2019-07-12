805aa7471475ead1a548ccf65ddd1e9ca45cf798
Emma Samms <47154306+EmmaDSamms@users.noreply.github.com>
2019-07-12 16:25:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Soviet Union</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="inc/css/main.css">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 class="rainbow">The Soviet Union</h1>
+        </div>
+        <div class="anthem">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/U06jlgpMtQs?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       </div>
+       <div class="info">
+           <center>
+                <br>
+                <br>
+                <p class="rainbow">Communism, <i>our</i> true way of living, we share everything and we shall be victorious(ignore the fact that we failed miserably)</p>
+            </center>
+        </div>
+        <div class="raspu">
+        <!-- <iframe width="0" height="0" src="https://www.youtube.com/embed/Oqi4ELiX5Us?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+        </div>
+        <div class="flag"> 
+            <!-- <img src="flag.jpeg" alt=""> -->
+        </div>
+    </div>
+</body>
+</html>
